3b7ef570ad44a6ecf075e94dc0f51f14af90115d
Eaglegrant <grantman522@gmail.com>
2024-07-06 15:30:20 -0400

    new skid

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -136,8 +136,8 @@ public class Vision extends SubsystemChecker {
 			return "Outside field";
 		}
 		double overallChange;
-		boolean[] moduleSkids = RobotContainer.drivetrainS.isSkidding();
-		if (moduleSkids[0] || moduleSkids[1] || moduleSkids[2] || moduleSkids[3]
+		boolean isSkidding = RobotContainer.drivetrainS.isSkidding();
+		if (isSkidding
 				|| RobotContainer.drivetrainS.isCollisionDetected() || override) {
 			if (RobotContainer.drivetrainS.isCollisionDetected()) {
 				override = true;
@@ -255,21 +255,18 @@ public class Vision extends SubsystemChecker {
 	 * deviation.
 	 */
 	private double applyAdditionalAdjustments(double stdDev, double tagCount) {
-		boolean[] moduleSkids = RobotContainer.drivetrainS.isSkidding();
-		for (boolean moduleSkid : moduleSkids) {
-			if (moduleSkid) {
-				stdDev /= 2;
-			}
+		boolean isSkidding = RobotContainer.drivetrainS.isSkidding();
+		if (isSkidding){
+			stdDev /= 4; 
 		}
 		if (RobotContainer.drivetrainS.isCollisionDetected() || override) {
 			stdDev /= 4;
 		}
-		if (moduleSkids[0] || moduleSkids[1] || moduleSkids[2]
-				|| moduleSkids[3]) {
-			return Math.max(0.05, stdDev);
+		if (isSkidding) { //overadjust from vision if skidding
+			return Math.max(0.00, stdDev);
 		}
 		// Add more adjustments as needed
-		return Math.max(0.02, stdDev); // Minimum threshold
+		return Math.max(0.05, stdDev); // Minimum threshold
 	}
 
 	/**
