8463f4c72f87719b7bb4db5e18ad9338203a951d
Eaglegrant <grantman522@gmail.com>
2024-10-25 22:21:27 -0400

    fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -524,7 +524,7 @@ public class RobotContainer {
 		selectButtonTest.onTrue(Commands.runOnce(SignalLogger::stop));
 		startButtonTest.onTrue(Commands.runOnce(SignalLogger::start));
 		if (Constants.currentMode == Mode.SIM) {
-			bButtonDrive.whileTrue(testOpponentRobot.getAutoCyleCommand());
+			//bButtonDrive.whileTrue(testOpponentRobot.getAutoCyleCommand());
 		}
 	}
 
