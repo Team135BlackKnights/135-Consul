b001d2fa65c1f7b188a3b212bb0408431aa7cd61
PaidShill <112586790+PaidShill@users.noreply.github.com>
2025-04-06 11:25:34 -0400

    SILENCE, MY STICK
    -No longer sends joystick disconnect warnings 24/7

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -103,6 +103,7 @@ public class Robot extends LoggedRobot {
 				e.printStackTrace();
 			}
 		}).start();
+		DriverStation.silenceJoystickConnectionWarning(true);
 		// Instantiate our RobotContainer. This will perform all our button bindings,
 		// and put our
 		// autonomous chooser on the dashboard
