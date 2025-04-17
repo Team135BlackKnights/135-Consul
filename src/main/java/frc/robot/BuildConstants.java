2d5517fc2e820c4470b211169e28418a98fc9e02
Eaglegrant <112586790+PaidShill@users.noreply.github.com>
2025-04-17 16:54:09 -0400

    CI Check
    Also branch update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "135-Blocks";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 634;
-   public static final String GIT_SHA = "d14df6c36a67a2f77b7a78efa9ff89c66830cfcc";
-   public static final String GIT_DATE = "2025-04-17 15:09:50 EDT";
-   public static final String GIT_BRANCH = "Proper-LED-2025";
-   public static final String BUILD_DATE = "2025-04-17 15:12:32 EDT";
-   public static final long BUILD_UNIX_TIME = 1744917152784L;
-   public static final int DIRTY = 1;
+   public static final int GIT_REVISION = 636;
+   public static final String GIT_SHA = "299b2ff1858a46b4137f469193741755f8d3908a";
+   public static final String GIT_DATE = "2025-04-17 16:48:11 EDT";
+   public static final String GIT_BRANCH = "LED_Code-CORE";
+   public static final String BUILD_DATE = "2025-04-17 16:52:35 EDT";
+   public static final long BUILD_UNIX_TIME = 1744923155316L;
+   public static final int DIRTY = 0;
 
    private BuildConstants(){}
 }
