4450986852ab64d530f0a923ace908426c1b5cb6
PaidShill <112586790+PaidShill@users.noreply.github.com>
2025-07-01 00:26:42 -0400

    Vendor Lib Updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "135-Blocks";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 465;
-   public static final String GIT_SHA = "75cade3b9498bc4cdb40d920ea9607ed21a3dd14";
-   public static final String GIT_DATE = "2025-06-26 17:52:21 EDT";
-   public static final String GIT_BRANCH = "2025-drive";
-   public static final String BUILD_DATE = "2025-06-27 14:45:05 EDT";
-   public static final long BUILD_UNIX_TIME = 1751049905644L;
+   public static final int GIT_REVISION = 475;
+   public static final String GIT_SHA = "86c69fd43ee10bd53c8c868d97833a378ff8d269";
+   public static final String GIT_DATE = "2025-06-30 23:56:42 EDT";
+   public static final String GIT_BRANCH = "drive-CORE";
+   public static final String BUILD_DATE = "2025-07-01 00:25:51 EDT";
+   public static final long BUILD_UNIX_TIME = 1751343951107L;
    public static final int DIRTY = 1;
 
    private BuildConstants(){}
