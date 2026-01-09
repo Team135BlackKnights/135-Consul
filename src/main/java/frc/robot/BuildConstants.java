c4989f798b1d796e3e296f44a6b0895ce1b69c14
Eaglegrant <grantman522@gmail.com>
2026-01-09 11:31:02 -0500

    the grind don't stop

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "135-Consul";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 496;
-   public static final String GIT_SHA = "e17a5e0abefe601b5942df6451c1d0aa5b23df65";
-   public static final String GIT_DATE = "2025-11-30 15:19:19 EST";
+   public static final int GIT_REVISION = 501;
+   public static final String GIT_SHA = "9296e900cb2098a2e2ed2eeb85a6ebb1db2ce7a1";
+   public static final String GIT_DATE = "2026-01-02 17:55:27 EST";
    public static final String GIT_BRANCH = "drive-CORE";
-   public static final String BUILD_DATE = "2025-12-25 01:49:17 EST";
-   public static final long BUILD_UNIX_TIME = 1766645357880L;
+   public static final String BUILD_DATE = "2026-01-09 11:27:56 EST";
+   public static final long BUILD_UNIX_TIME = 1767976076358L;
    public static final int DIRTY = 1;
 
    private BuildConstants(){}
