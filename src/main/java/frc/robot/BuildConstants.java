112b90e069c6a5e4aeab0921bb32ed268aa540bd
driverstation2 <robotics@phm.k12.in.us>
2026-09-09 15:03:41 -0400

    Revert "Constants for 328 2025"
    This reverts commit 6c079764b93c0acc575fe298ceb20dfb66f6f621.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "135-Consul";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 503;
-   public static final String GIT_SHA = "b089dfd3c5f815550ec91158262a950c2ef17d43";
-   public static final String GIT_DATE = "2026-05-13 16:27:15 EDT";
-   public static final String GIT_BRANCH = "tShirt2026";
-   public static final String BUILD_DATE = "2026-09-04 14:43:43 EDT";
-   public static final long BUILD_UNIX_TIME = 1788547423698L;
-   public static final int DIRTY = 0;
+   public static final int GIT_REVISION = 502;
+   public static final String GIT_SHA = "c4989f798b1d796e3e296f44a6b0895ce1b69c14";
+   public static final String GIT_DATE = "2026-01-09 11:31:02 EST";
+   public static final String GIT_BRANCH = "drive-CORE";
+   public static final String BUILD_DATE = "2026-05-13 16:26:51 EDT";
+   public static final long BUILD_UNIX_TIME = 1778704011751L;
+   public static final int DIRTY = 1;
 
    private BuildConstants(){}
 }
