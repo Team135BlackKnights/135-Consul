a0a15c1a6bd6cb82f48204ccb8751ed5b21bdf8e
driverstation2 <robotics@phm.k12.in.us>
2026-09-11 14:31:36 -0400

    yo

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
-   public static final String MAVEN_NAME = "135-Blocks";
+   public static final String MAVEN_NAME = "135-Consul";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 475;
-   public static final String GIT_SHA = "86c69fd43ee10bd53c8c868d97833a378ff8d269";
-   public static final String GIT_DATE = "2025-06-30 23:56:42 EDT";
-   public static final String GIT_BRANCH = "drive-CORE";
-   public static final String BUILD_DATE = "2025-07-01 00:25:51 EDT";
-   public static final long BUILD_UNIX_TIME = 1751343951107L;
+   public static final int GIT_REVISION = 487;
+   public static final String GIT_SHA = "a51b6a020ef03a12741b2ba55bac69b6a3a0a569";
+   public static final String GIT_DATE = "2026-09-09 16:54:05 EDT";
+   public static final String GIT_BRANCH = "tShirt-v2025";
+   public static final String BUILD_DATE = "2026-09-09 17:35:49 EDT";
+   public static final long BUILD_UNIX_TIME = 1788989749040L;
    public static final int DIRTY = 1;
 
    private BuildConstants(){}
